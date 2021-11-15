16a23694f0e86b2d2897dd97582130c3e88c28ab
Lai JunBin <laijunbin@TW-LW60450-M.local>
2021-11-15 14:31:05 +0800

    Add tailwindcss.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-[#39f] text-white text-3xl text-center py-3 select-none">
+      Hello World
     </div>
   )
 }
